125ff83025b4e6fbf6bb6afa7f7ded83ba6ade3b
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-04-18 09:03:13 +0300

    refactoring Categories.jsx add onClick handler

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+const CATEGORIES = [
+  "Все",
+  "Мясные",
+  "Вегетарианская",
+  "Гриль",
+  "Острые",
+  "Закрытые",
+];
 
 export const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {CATEGORIES.map((nameCategory, index) => {
+          return (
+            <li
+              key={index}
+              className={activeIndex === index ? "active" : ""}
+              onClick={() => setActiveIndex(index)}
+            >
+              {nameCategory}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
