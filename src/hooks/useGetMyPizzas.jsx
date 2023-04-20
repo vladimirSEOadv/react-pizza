86bef18b179fa774f87135f68fc39705462b2d58
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-04-20 12:46:36 +0300

    Create useGetMyPizzas hook

---
+++ src/hooks/useGetMyPizzas.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const useGetMyPizzas = (link) => {
+  const [pizzas, setPizzas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(link)
+      .then((data) => {
+        const { ok, status, statusText } = data;
+        if (!ok) throw new Error(`${status} ${statusText}`);
+        return data.json();
+      })
+      .then((json) => {
+        setPizzas(json);
+      })
+      .catch((err) => {
+        console.log(err, "in useGetMyPizzas hook");
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, [link]);
+
+  return [pizzas, loading, error];
+};
