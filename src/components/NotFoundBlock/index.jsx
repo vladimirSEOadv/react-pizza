3ed686cfc66915557a5c7dffc2c9f2196d1e4764
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-04-21 08:42:25 +0300

    create page NotFound

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./NotFoundBlock.module.scss";
+
+export const NotFoundBlock = () => {
+  return (
+    <>
+      <div className={styles.root}>
+        <span>😕</span>
+        <h1>Ничего не найдено</h1>
+        <p className={styles.description}>
+          К сожалению данная станица отсутствует в нашем магазине
+        </p>
+      </div>
+    </>
+  );
+};
