a8e6ba0585a9110d74d5b1ea35002b39af892dfd
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-04-21 08:44:46 +0300

    create connect pages and browser router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import "./scss/app.scss";
 import { Header } from "./components/Header";
-import { Categories } from "./components/Categories";
-import { Sort } from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import { useGetMyPizzas } from "./hooks/useGetMyPizzas";
-import { Loader } from "./components/Loader";
-import Skeleton from "./components/PizzaBlock/Skeleton";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Cart } from "./pages/Cart";
+import { NotFound } from "./pages/NotFound";
+import React from "react";
 
 function App() {
-  const [pizzas, loading, error] = useGetMyPizzas(
-    "https://6436dc673e4d2b4a12dda417.mockapi.io/items"
-  );
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {error && <Loader error={error} />}
-            {loading &&
-              [...new Array(6)].map((_, index) => {
-                return <Skeleton key={index} />;
-              })}
-            {pizzas.length > 0 &&
-              pizzas.map((pizza) => {
-                return <PizzaBlock {...pizza} key={pizza.id} />;
-              })}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
