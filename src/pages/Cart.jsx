5577a8b2c0a6739a5108104491dd0dd315ac30f2
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-04-26 09:39:21 +0300

    remove go-back icon to separate directory, fix styles

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import goBacksvg from "../assets/img/go-back.svg";
 
 export const Cart = () => {
   return (
@@ -172,21 +173,7 @@ export const Cart = () => {
               to="/"
               className="button button--outline button--add go-back-btn"
             >
-              <svg
-                width="8"
-                height="14"
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 13L1 6.93015L6.86175 1"
-                  stroke="#D3D3D3"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+              <img src={goBacksvg} alt="goBacksvg" />
               <span>Вернуться назад</span>
             </Link>
             <div className="button pay-btn">
