8ebcd18475eba516454129a92d25686e8200344e
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-05-29 06:35:57 +0300

    refactor: useCreateUrlParams.js remove push search params to url into a separate function

--- src/hooks/useGetMyPizzas/useCreateUrlParams.js
+++ src/hooks/useGetMyPizzas/useCreateUrlParams.js
@@ -1,35 +1,13 @@
 import { sortValues } from "../../constants/sortValues";
 import { useSelector } from "react-redux";
-import qs from "qs";
-import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { usePushParamsToUrl } from "./usePushParamsToUrl";
 
 export const useCreateUrlParams = () => {
-  const navigate = useNavigate();
-  const isMounted = useRef(false);
   const { sortIndex, categoryIndex, searchQuery } = useSelector(
     (state) => state.filters
   );
 
-  useEffect(() => {
-    if (isMounted.current) {
-      const queryString =
-        searchQuery === "" || searchQuery === undefined
-          ? qs.stringify({
-              category: categoryIndex || "*",
-              orderBy: sortValues[sortIndex].orderBy,
-              order: sortValues[sortIndex].order,
-            })
-          : qs.stringify({
-              search: `${searchQuery}`,
-              orderBy: sortValues[sortIndex].orderBy,
-              order: sortValues[sortIndex].order,
-            });
-      navigate(`?${queryString}`);
-    } else {
-      isMounted.current = true;
-    }
-  }, [sortIndex, categoryIndex, searchQuery, navigate]);
+  usePushParamsToUrl(sortIndex, categoryIndex, searchQuery);
 
   const category = `category=${categoryIndex || "*"}`;
   const search = `&search=${searchQuery}`;
---
+++ src/hooks/useGetMyPizzas/usePushParamsToUrl.js
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import qs from "qs";
+import { sortValues } from "../../constants/sortValues";
+
+export const usePushParamsToUrl = (sortIndex, categoryIndex, searchQuery) => {
+  const navigate = useNavigate();
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString =
+        searchQuery === "" || searchQuery === undefined
+          ? qs.stringify({
+              category: categoryIndex || "*",
+              orderBy: sortValues[sortIndex].orderBy,
+              order: sortValues[sortIndex].order,
+            })
+          : qs.stringify({
+              search: `${searchQuery}`,
+              orderBy: sortValues[sortIndex].orderBy,
+              order: sortValues[sortIndex].order,
+            });
+      navigate(`?${queryString}`);
+    } else {
+      isMounted.current = true;
+    }
+  }, [sortIndex, categoryIndex, searchQuery, navigate]);
+};
