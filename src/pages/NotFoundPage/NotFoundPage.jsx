53967f10764c5f6b848b0408eec0afd4cc513873
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-06-09 06:49:20 +0300

    change NotFoundPage.jsx.jsx and NotFoundBlock to NotFoundPage.tsx and NotFoundBlock.tsx

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { NotFoundBlock } from "../../components/NotFoundBlock";
-
-export const NotFoundPage = () => {
-  return <NotFoundBlock />;
-};
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { NotFoundBlock } from "../../components/NotFoundBlock/NotFoundBlock";
+
+export const NotFoundPage: React.FC = () => {
+  return <NotFoundBlock />;
+};
